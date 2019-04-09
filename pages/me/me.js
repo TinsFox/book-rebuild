c9ce6e8a06094d1dfc68a4b94ade2ed2eb19711b
limbo <limbo@limbos-MacBook-Pro.local>
2019-04-09 11:12:06 +0800

    修改ui

--- pages/me/me.js
+++ pages/me/me.js
@@ -9,14 +9,14 @@ var student = [
   { "name": "已购教材", "ico": "database.png", "url": "../yigou/yigou" },
   { "name": "我的评价", "ico": "edit.png", "url": "../myevaluate_stu/myevaluate_stu" },
   { "name": "意见反馈", "ico": "edit.png", "url": "../suggestion/suggestion" },
-  { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin"},
+  // { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin"},
 ];
 var teacher = [
   //教师
   { "name": "我的教材", "ico": "edits.png", "url": "../mybook/mybook" },
   { "name": "我的评价", "ico": "edit.png", "url": "../myevaluate_stu/myevaluate_stu" },
   { "name": "意见反馈", "ico": "edit.png", "url": "../suggestion/suggestion" },
-  { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
+  // { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
 ];
 var ac = [
   //教务员
@@ -26,17 +26,21 @@ var ac = [
   { "name": "查看教学计划 ", "ico": "book.png", "url": "../teacherplan/teacherplan" },
   { "name": "查看教材信息 ", "ico": "book.png", "url": "../bookmessage/bookmessage" },
   { "name": "意见反馈", "ico": "edit.png", "url": "../suggestion/suggestion" },
-  { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
+  // { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
 ];
 var leader = [
   //领导
   // { "name": "查看教学计划 ", "ico": "book.png", "url": "../teacherplan/teacherplan" },
   // { "name": "查看教材信息 ", "ico": "book.png", "url": "../bookmessage/bookmessage" },
-  { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
+  // { "name": "退出登录", "ico": "getInfo.png", "url": "../clearLogin/clearLogin" }
 ];
+
 Page({
   data: {
-    grids: grids
+    grids: grids,
+    other: [
+      { "name": "退出登录", "ico": "getInfo.png", "url": "exit" }
+    ]
   },
   onLoad: function () {
     wx.checkSession({
@@ -76,5 +80,21 @@ Page({
         grids: leader
       })
     }
+  },
+  exit: function(){
+    wx.showModal({
+      title: '退出登录',
+      content: '请选择',
+      success: function (res) {
+        if (res.confirm) {
+          wx.clearStorageSync()
+          wx.reLaunch({
+            url: '../login/login'
+          })
+        } else if (res.cancel) {
+          wx.navigateBack();
+        }
+      }
+    })
   }
 })
